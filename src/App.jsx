cb04bd62054877283e7d9aff23130826d0e11a19
Matthew Bixby <matthewbixby@matthews-air.myfiosgateway.com>
2024-02-09 18:11:14 -0500

    added puppy data with useState, Used the Data in React, and moved over data with map

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,23 @@
-import './App.css'
+import React, { useState } from 'react';
+
+import './App.css';
+import {puppyList} from './data.js';
+
 
 function App() {
-  
+  const [puppies, setPuppies] = useState(puppyList);
+  // Log the puppyList arrray to the console (below)
+  console.log("puppyList: ", puppyList);
 
   return (
-    <>
- 
-    </>
-  )
+    <div className="App">
+      {
+        puppies.map((puppy) => {
+          return <p key={puppy.id}>{puppy.name}</p>;
+        })
+      }
+    </div>
+  );
 }
 
 export default App
